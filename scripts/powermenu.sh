fa387154da22334d71183959120a13e844dbf90e
qwdsx <qwdsx@>
2023-03-20 17:55:19 +0200

    remove i3 exit thing

--- scripts/powermenu.sh
+++ scripts/powermenu.sh
@@ -23,11 +23,7 @@ option=$(echo -e "$options" | rofi -i -dmenu -p "power menu" -theme $rofi_theme)
 
 case $option in
     $logout)
-        #if [[ "$DESKTOP_SESSION" == "bspwm" ]]; then
-            bspc quit
-        #elif [[ "$DESKTOP_SESSION" == "i3" ]]; then
-            i3-msg exit
-        #fi
+        bspc quit
         ;;
     $shutdown)
         systemctl poweroff
